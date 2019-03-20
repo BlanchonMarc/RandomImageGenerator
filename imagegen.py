dd52c60c1fbbeda49001a5bc45ffb41ccdafba9c
BlanchonMarc <fr.marc.blanchon@gmail.com>
2019-03-20 15:57:50 +0100

    perlin noise

--- imagegen.py
+++ imagegen.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.draw import random_shapes
+import noise
+import random
 
 
 def generateRandShapes(min_s, max_s, min_size, overlap, sizes=(1000, 1000)):
@@ -8,3 +10,51 @@ def generateRandShapes(min_s, max_s, min_size, overlap, sizes=(1000, 1000)):
                              min_size=min_size, allow_overlap=overlap, num_trials=100)
 
     return image
+
+
+def GeneratePerlin(scale=100, octaves=6, persistence=0.5, lacunarity=2.0):
+    shape = (1000, 1000)
+    seed = np.random.randint(0, 100)
+
+    world = np.zeros(shape)
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            world[i][j] = noise.pnoise2(i/scale,
+                                        j/scale,
+                                        octaves=octaves,
+                                        persistence=persistence,
+                                        lacunarity=lacunarity,
+                                        repeatx=1024,
+                                        repeaty=1024,
+                                        base=seed)
+    color_world = np.zeros(world.shape+(3,))
+
+    color1 = [random.randint(0, 256),
+              random.randint(0, 256),
+              random.randint(0, 256)]
+    color2 = [random.randint(0, 256),
+              random.randint(0, 256),
+              random.randint(0, 256)]
+    color3 = [random.randint(0, 256),
+              random.randint(0, 256),
+              random.randint(0, 256)]
+    color4 = [random.randint(0, 256),
+              random.randint(0, 256),
+              random.randint(0, 256)]
+    color5 = [random.randint(0, 256),
+              random.randint(0, 256),
+              random.randint(0, 256)]
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            if world[i][j] < -0.05:
+                color_world[i][j] = color1
+            elif world[i][j] < 0:
+                color_world[i][j] = color2
+            elif world[i][j] < .20:
+                color_world[i][j] = color3
+            elif world[i][j] < 0.35:
+                color_world[i][j] = color4
+            elif world[i][j] < 1.0:
+                color_world[i][j] = color5
+
+    return color_world
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     if not os.path.exists(args.output):
         os.makedirs(args.output[:-1])
     # set the display level to none.
-    display = False
+    display = True
 
     min_shape = random.randint(1, 101)
     max_shape = random.randint(min_shape+1, 201)
@@ -29,22 +29,28 @@ if __name__ == "__main__":
     min_size = random.randint(1, 10)
 
     boolOverlap = bool(random.getrandbits(1))
-    try:
+    counter = 0
+    while counter < int(args.number):
+        print(int(args.number))
+        print(counter)
+        print(counter < int(args.number))
+
         generated_Image = imagegen.generateRandShapes(
-            min_s=min_shape, max_s=max_shape, min_size=min_size, overlap=boolOverlap)
-    except Exception:
-        print(f"Error In the Shape Creation")
-    #cv2.cvtColor(generated_Image, cv2.COLOR_RGB2HSV)
-    generated_Image = color.rgb_to_hsv(generated_Image)
-    if diplay:
-        print(f"shape is {generated_Image.shape}")
-        print(f"max val is {np.amax(generated_Image[:,:,1])}")
-        plt.imshow(generated_Image)
-        plt.show()
+            min_s=min_shape, max_s=max_shape, min_size=min_size, overlap=True)
 
-    np.save(f"{args.output}osef.npy", generated_Image)
+        generated_Image = imagegen.GeneratePerlin()
 
-    if display:
-        loaded = np.load(f"{args.output}osef.npy")
-        plt.imshow(loaded)
-        plt.show()
+        generated_Image = color.rgb_to_hsv(generated_Image)
+        if display:
+            print(f"shape is {generated_Image.shape}")
+            print(f"max val is {np.amax(generated_Image[:,:,1])}")
+            plt.imshow(generated_Image)
+            plt.show()
+        name = format(counter, "07d")
+        np.save(f"{args.output}{name}.npy", generated_Image)
+
+        if display:
+            loaded = np.load(f"{args.output}{name}.npy")
+            plt.imshow(loaded)
+            plt.show()
+        counter += 1
